33443bacb9631e6df69dfc9a7fc9fed226aed6f8
idan.rom <idanrommm@gmail.com>
2023-05-02 20:35:36 +0300

    char literals replaced with constants, added convenient implementations with sstream

--- json/src/json_string.cpp
+++ json/src/json_string.cpp
@@ -1,4 +1,5 @@
 #include <stdexcept>
+#include <sstream>
 
 #include "json_string.hpp"
 #include "json_types.hpp"
@@ -15,29 +16,29 @@ JsonString::JsonValueType JsonString::getType() const {
 }
 
 std::string JsonString::getJsonString() const {
-	std::string output_string("");
+	std::stringstream output_string_stream;
 	
-	output_string += (char)(JsonSpecialChar::STRING_START) + m_value + (char)(JsonSpecialChar::STRING_END);
-	return output_string;
+	output_string_stream << (char)(JsonSpecialChar::STRING_START) << m_value << (char)(JsonSpecialChar::STRING_END);
+	return output_string_stream.str();
 }
 
 const char *JsonString::parse(const char * const from) {
 	if (nullptr == from) {
 		throw std::invalid_argument("nullptr reveived");
 	}
-	const char *iter = skipChars(from, ' ');
+	const char *iter = skipChars(from, (char)(JsonSpecialChar::SPACE));
 	if ((char)(JsonSpecialChar::STRING_START) != *iter) {
 		throw std::invalid_argument("invalid string start");
 	}
 	++iter;
-	std::string value = "";
+	std::stringstream value_stream;
 	while (!('\0' == *iter)) {
 		if ((char)(JsonSpecialChar::STRING_END) == *iter) {
-			m_value = value;
+			m_value = value_stream.str();
 			++iter;
 			return iter;
 		}
-		value += *iter;
+		value_stream << *iter;
 		++iter;
 	}
 	throw std::invalid_argument("invalid string end");
