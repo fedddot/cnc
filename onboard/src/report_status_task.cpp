4e5f5528da2c01e678a0ca6876f173d29538cae9
idan.rom <idanrommm@gmail.com>
2023-05-08 20:14:18 +0300

    Onboard tasks implementation started

---
+++ onboard/src/report_status_task.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include "report_status_task.hpp"
+
+using namespace onboard;
+
+ReportStatusTask::ReportStatusTask(const std::string& status) : m_status(status) {
+
+}
+
+ReportStatusTask::McuTaskType ReportStatusTask::getType() const {
+	return McuTaskType::REPORT_STATUS;
+}
+
+void ReportStatusTask::execute() {
+	std::cout << m_status << std::endl;
+}
