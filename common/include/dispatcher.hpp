3e16c2f0364d3029fcda79f09b1ff24a5b714e26
idan.rom <idanrommm@gmail.com>
2023-05-13 20:28:28 +0300

    prepared to be implemented in terms of data::List

--- common/include/dispatcher.hpp
+++ common/include/dispatcher.hpp
@@ -1,44 +1,40 @@
 #ifndef	__DISPATCHER_HPP__
 #define	__DISPATCHER_HPP__
 
-#include <set>
-#include <algorithm>
+#include "list.hpp"
+#include "shared_ptr.hpp"
 #include "ilistener.hpp"
 
 namespace common {
-
 	template <class Event>
-	class Dispatcher: public IListener<Event>
-	{
+	class Dispatcher: public IListener<Event> {
 	public:
+		Dispatcher() = default;
+		Dispatcher(const Dispatcher<T>& other) = delete;
+		Dispatcher& operator=(const Dispatcher<T>& other) = delete;
+		~Dispatcher() noexcept = default;
+		
 		virtual void onEvent(const Event& event) override;
-		virtual void subscribe(const IListener<Event> *listener);
-		virtual void unsubscribe(const IListener<Event> *listener);
+		virtual void subscribe(const data::SharedPtr<IListener<Event>> listener);
 	private:
-		std::set<IListener<Event> *> listeners_;
+		data::List<data::SharedPtr<IListener<Event>>> m_listeners;
 	};
 
 	template <class Event>
 	void Dispatcher<Event>::onEvent(const Event& event) {
-		std::for_each(
-			listeners_.begin(),
-			listeners_.end(),
-			[&](IListener<Event> *curr_listener) {
-				curr_listener->onEvent(event);
-			}
-		);
+		// std::for_each(
+		// 	m_listeners.begin(),
+		// 	m_listeners.end(),
+		// 	[&](IListener<Event> *curr_listener) {
+		// 		curr_listener->onEvent(event);
+		// 	}
+		// );
 	}
 
 	template <class Event>
-	void Dispatcher<Event>::subscribe(const IListener<Event> *listener) {
-		listeners_.insert(const_cast<IListener<Event> *>(listener));
+	void Dispatcher<Event>::subscribe(const data::SharedPtr<IListener<Event>> listener) {
+		// m_listeners.insert(const_cast<IListener<Event> *>(listener));
 	}
-
-	template <class Event>
-	void Dispatcher<Event>::unsubscribe(const IListener<Event> *listener) {
-		listeners_.erase(const_cast<IListener<Event> *>(listener));
-	}
-
 }
 
-#endif
+#endif // __DISPATCHER_HPP__
