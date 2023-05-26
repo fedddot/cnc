e4782be8fdba8049c4a9ba24989c9506b5225e6b
idan.rom <idanrommm@gmail.com>
2023-05-26 08:16:51 +0300

    Parsing from list instead of char *

--- common/include/iparsable.hpp
+++ common/include/iparsable.hpp
@@ -1,12 +1,14 @@
 #ifndef	__IPARSABLE_HPP__
 #define	__IPARSABLE_HPP__
 
+#include "list.hpp"
+
 namespace common
 {
 	class IParsable {
 	public:
 		virtual ~IParsable() noexcept = 0;
-		virtual const char *parse(const char * const from) = 0;
+		virtual data::List<char>::Iter parse(const data::List<char>::Iter& start) = 0;
 	}; // IParsable
 } // common
 
