4dfc83485cad920891e5490ba5bd500b47b60408
idan.rom <idanrommm@gmail.com>
2023-05-26 11:07:41 +0300

    Reimplemented

--- json/include/json_array.hpp
+++ json/include/json_array.hpp
@@ -1,33 +1,18 @@
 #ifndef	__JSON_ARRAY_HPP__
 #define	__JSON_ARRAY_HPP__
 
-#include <string>
-#include <cstddef>
-#include <vector>
-#include <memory>
-
 #include "ijson_value.hpp"
+#include "list.hpp"
+#include "string.hpp"
+#include "shared_ptr.hpp"
 
 namespace json {
-	class JsonArray: public IJsonValue {
+	class JsonArray: public IJsonValue, public data::List<data::SharedPtr<IJsonValue>> {
 	public:
 		virtual JsonValueType getType() const override;
-		virtual std::string getJsonString() const override;
-		virtual const char *parse(const char * const from) override;
-		void addObject(const std::shared_ptr<IJsonValue>& value);
-		
-		inline std::shared_ptr<IJsonValue> get(const std::size_t index) const;
-	private:
-		std::vector<std::shared_ptr<IJsonValue>> m_values;
-	};
-
-	inline std::shared_ptr<IJsonValue> JsonArray::get(const std::size_t index) const {
-		if (m_values.size() > index) {
-			return m_values[index];
-		}
-		return nullptr;
-	}
-
-}
+		virtual data::String getJsonString() const override;
+		virtual data::List<char>::Iter parse(const data::List<char>::Iter& start) override;
+	}; // class JsonArray
+} // namespace json
 
 #endif // __JSON_ARRAY_HPP__
