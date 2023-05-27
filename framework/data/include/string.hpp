c701ca093727ea042349fd912c349cf580d559f4
idan.rom <idanrommm@gmail.com>
2023-05-27 22:40:42 +0300

    class foot print optimized

--- framework/data/include/string.hpp
+++ framework/data/include/string.hpp
@@ -10,19 +10,44 @@ namespace data {
 		String(const char& chr);
 		String(const String& other);
 		virtual ~String() noexcept;
-		virtual String& operator=(const String& other);
-		virtual String& operator+=(const String& other);
-		virtual String& operator+=(const char& chr);
-		virtual const char *c_str() const;
-		virtual size_t size() const;
+		String& operator=(const String& other);
+		String operator+(const String& other) const;
+		inline String& operator+=(const String& other);
+		inline String& operator+=(const char& chr);
+		inline const char *c_str() const;
+		inline size_t size() const;
+		inline bool operator==(const String& other) const;
 	private:
 		char *m_string;
-		static char *copyString(const char * const str);
+		static char *strDup(const char * const src);
+		static char *strCpy(char * const dst, const char * const src);
+		static size_t strLen(const char * const str);
+		static int strCmp(const char * const one, const char * const other);
+		static char *strCat(const char * const one, const char * const other);
 	}; // String
 
-	String operator+(const String& left, const String& right);
-	bool operator==(const String& left, const String& right);
-	
+	inline String& String::operator+=(const String& other) {
+		*this = *this + other;
+		return *this;
+	}
+
+	inline String& String::operator+=(const char& chr) {
+		*this = *this + String(chr);
+		return *this;
+	}
+
+	inline const char *String::c_str() const {
+		return m_string;
+	}
+
+	inline size_t String::size() const {
+		return strLen(m_string);
+	}
+
+	inline bool String::operator==(const String& other) const {
+		return (0 == strCmp(m_string, other.m_string));
+	}
+
 } // namespace data
 
 #endif // __STRING_HPP__
--- framework/data/src/string.cpp
+++ framework/data/src/string.cpp
@@ -1,4 +1,3 @@
-#include <string.h>
 #include "string.hpp"
 #include "exception.hpp"
 #include "exception_handler.hpp"
@@ -8,84 +7,97 @@ using namespace data;
 static const String invalidPointerMsg("inv_ptr");
 static const String failedCopyMsg("cpy_fld");
 
-String::String(const char * const str) : m_string(copyString(str)) {
+String::String(const char * const str) : m_string(strDup(str)) {
 
 }
 
-String::String(const char& chr) : m_string(copyString(" ")) {
+String::String(const char& chr) : m_string(strDup(" ")) {
 	m_string[0] = chr;
 }
 
-String::String(const String& other) : m_string(copyString(other.m_string)) {
+String::String(const String& other) : m_string(strDup(other.m_string)) {
 
-}
-
-String& String::operator=(const String& other) {
-	char *string_buff = copyString(other.m_string);
-	delete[] m_string;
-	m_string = string_buff;
-	string_buff = nullptr;
-	return *this;
 }
 
 String::~String() noexcept {
 	delete[] m_string;
 }
 
-String& String::operator+=(const String& other) {
-	const size_t left_size = size();
-	const size_t right_size = other.size();
-	char *buff = new char[left_size + right_size + 1UL];
-	if ((nullptr == strcpy(buff, c_str())) || (nullptr == strcpy(buff + left_size, other.c_str()))) {
-		except::ExceptionHandler::getInstance()->onEvent(except::Exception(failedCopyMsg));
-	}
-	*(buff + left_size + right_size) = '\0';
-	delete m_string;
-	m_string = buff;
+String& String::operator=(const String& other) {
+	char *string_buff = strDup(other.m_string);
+	delete[] m_string;
+	m_string = string_buff;
+	string_buff = nullptr;
 	return *this;
 }
 
-String& String::operator+=(const char& chr) {
-	const size_t left_size = size();
-	const size_t right_size = 1UL;
-	char *buff = new char[left_size + right_size + 1UL];
-	if (nullptr == strcpy(buff, c_str())) {
-		except::ExceptionHandler::getInstance()->onEvent(except::Exception(failedCopyMsg));
-	}
-	*(buff + left_size) = chr;
-	*(buff + left_size + right_size) = '\0';
-	delete m_string;
-	m_string = buff;
-	return *this;
-}
-
-const char *String::c_str() const {
-	return m_string;
-}
-
-size_t String::size() const {
-	return strlen(m_string);
-}
-
-char *String::copyString(const char * const str) {
-	if (nullptr == str) {
-		except::ExceptionHandler::getInstance()->onEvent(except::Exception(invalidPointerMsg));
-	}
-	char *buff = new char[strlen(str) + 1UL];
-	if (nullptr == strcpy(buff, str)) {
-		delete[] buff;
-		buff = nullptr;
-		except::ExceptionHandler::getInstance()->onEvent(except::Exception(failedCopyMsg));
-	}
-	return buff;
-}
-
-String data::operator+(const String& left, const String& right) {
-	String result(left);
-	result += right;
+String String::operator+(const String& other) const {
+	char *buff = strCat(m_string, other.m_string);
+	String result(buff);
+	delete[] buff;
+	buff = nullptr;
 	return result;
 }
 
-bool data::operator==(const String& left, const String& right) {
-	return (0 == strcmp(left.c_str(), right.c_str()));
+char *String::strDup(const char * const str) {
+	if (nullptr == str) {
+		except::ExceptionHandler::getInstance()->onEvent(except::Exception(invalidPointerMsg));
+	}
+	size_t str_size = strLen(str);
+	char *buff = new char[str_size + 1UL];
+	strCpy(buff, str);
+	*(buff + str_size) = '\0';
+	return buff;
+}
+
+char *String::strCpy(char * const dst, const char * const src) {
+	if ((nullptr == dst) || (nullptr == src)){
+		except::ExceptionHandler::getInstance()->onEvent(except::Exception(invalidPointerMsg));
+	}
+	const char *src_char_iter = src;
+	char *dst_char_iter = dst;
+	while ('\0' != *src_char_iter) {
+		*dst_char_iter = *src_char_iter;
+		++src_char_iter;
+		++dst_char_iter;
+	}
+	return dst;
+}
+
+size_t String::strLen(const char * const str) {
+	if (nullptr == str) {
+		except::ExceptionHandler::getInstance()->onEvent(except::Exception(invalidPointerMsg));
+	}
+	const char *str_char_iter = str;
+	while ('\0' != *str_char_iter) {
+		++str_char_iter;
+	}
+	return str_char_iter - str;
+}
+
+int String::strCmp(const char * const one, const char * const other) {
+	if ((nullptr == one) || (nullptr == other)){
+		except::ExceptionHandler::getInstance()->onEvent(except::Exception(invalidPointerMsg));
+	}
+	const char *src_char_iter = one;
+	const char *dst_char_iter = other;
+	int result = 0;
+	while (0 == (result = static_cast<int>(*src_char_iter) - static_cast<int>(*dst_char_iter))) {
+		if (('\0' == *src_char_iter) || ('\0' == *dst_char_iter)) {
+			break;
+		}		
+		++src_char_iter;
+		++dst_char_iter;
+	}
+	return result;
+}
+
+char *String::strCat(const char * const one, const char * const other) {
+	size_t one_len = strLen(one);
+	size_t other_len = strLen(other);
+	char *result = new char[one_len + other_len + 1];
+	strCpy(result, one);
+	strCpy(result + one_len, other);
+	*(result + one_len + other_len) = '\0';
+	return result;
 }
