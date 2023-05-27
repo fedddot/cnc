507112175165b0d3fc1928c02e2eae19e763f77a
idan.rom <idanrommm@gmail.com>
2023-05-27 16:10:30 +0300

    tests fixed

--- framework/message_parser/tests/src/test_message_listener.cpp
+++ framework/message_parser/tests/src/test_message_listener.cpp
@@ -11,6 +11,8 @@ using namespace data;
 void TestMessageListener::onEvent(const List<char>& event) {
 	auto iter = const_cast<List<char>&>(event).begin();
 	while (!iter.isEndIter()) {
-		std::cout << iter.get() << std::endl;
-	}	
+		std::cout << iter.get();
+		++iter;
+	}
+	std::cout << std::endl;
 }
