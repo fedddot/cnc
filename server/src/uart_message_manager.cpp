d81c34c5c71e1fe26852bd3e77d9c7c7ff755d69
idan.rom <idanrommm@gmail.com>
2023-06-02 22:17:55 +0300

    itendation fix, redundant static var

--- server/src/uart_message_manager.cpp
+++ server/src/uart_message_manager.cpp
@@ -11,29 +11,28 @@
 using namespace cnc_system;
 
 static UartMessageManager *currentMessageManager = nullptr;
-static int chars_rxed = 0;
 
 void on_uart_rx() {
 	if (nullptr == currentMessageManager) {
 		return;
 	}
-    while (uart_is_readable(uart0)) {
+	while (uart_is_readable(uart0)) {
 		currentMessageManager->onEvent((char)uart_getc(uart0));
 	}
 }
 
 UartMessageManager::UartMessageManager(const std::vector<char>& start_signature, const std::size_t& length_field_size, const std::size_t& baud_rate): MessageManager(start_signature, length_field_size), m_baud_rate(baud_rate) {
 	uart_init(uart0, (uint)m_baud_rate);
-    irq_set_exclusive_handler(UART0_IRQ, on_uart_rx);
-    irq_set_enabled(UART0_IRQ, true);
-    uart_set_irq_enables(uart0, true, false);
+	irq_set_exclusive_handler(UART0_IRQ, on_uart_rx);
+	irq_set_enabled(UART0_IRQ, true);
+	uart_set_irq_enables(uart0, true, false);
 	currentMessageManager = this;
 }
 
 UartMessageManager::~UartMessageManager() noexcept {
 	irq_remove_handler(UART0_IRQ, on_uart_rx);
-    irq_set_enabled(UART0_IRQ, false);
-    uart_set_irq_enables(uart0, false, false);
+	irq_set_enabled(UART0_IRQ, false);
+	uart_set_irq_enables(uart0, false, false);
 	uart_deinit(uart0);
 	currentMessageManager = nullptr;
 }
