6c5d47c710c3a24fc3568ddf20639a524efe53e0
idan.rom <idanrommm@gmail.com>
2023-07-29 12:37:43 +0300

    wip

--- framework/json/include/json_parser.hpp
+++ framework/json/include/json_parser.hpp
@@ -1,18 +1,21 @@
 #ifndef	__JSON_PARSER_HPP__
 #define	__JSON_PARSER_HPP__
 
-#include <cstddef>
-#include <vector>
-#include <string>
+#include <iostream>
 #include <memory>
 
 #include "idata.hpp"
 #include "idata_parser.hpp"
 
 namespace json {
-	class JsonParser: public IDataParser {
+	class JsonParser: public data::IDataParser {
 	public:
-		virtual std::shared_ptr<IData> parse(std::istream& data_stream) override;
+		virtual std::shared_ptr<data::IData> parse(std::istream& data_stream) override;
+	private:
+		std::shared_ptr<data::IData> parseString(std::istream& data_stream);
+
+		static char peekChar(std::istream& data_stream);
+		static char popChar(std::istream& data_stream);
 	};
 }
 
--- framework/json/src/json_parser.cpp
+++ framework/json/src/json_parser.cpp
@@ -1,3 +1,21 @@
+#include <iostream>
+#include <memory>
+
+#include "idata.hpp"
+#include "idata_parser.hpp"
+
+#include "json_common.hpp"
 #include "json_parser.hpp"
 
 using namespace json;
+using namespace data;
+
+std::shared_ptr<IData> JsonParser::parse(std::istream& data_stream) {
+
+}
+
+std::shared_ptr<data::IData> parseString(std::istream& data_stream) {
+	if (JsonSpecialChar::STRING_START != peekChar(data_stream)) {
+		throw
+	}
+}
