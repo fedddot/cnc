35a45ab2472d8c9849914c034c86cf6959d88005
idan.rom <idanrommm@gmail.com>
2023-08-07 22:11:34 +0300

    data receiver implemented

--- client/include/data_receiver.hpp
+++ client/include/data_receiver.hpp
@@ -4,7 +4,7 @@
 #include <stdexcept>
 #include <string>
 #include <cstddef>
-#include <iostream>
+#include <vector>
 #include "ilistener.hpp"
 
 namespace data {
@@ -12,7 +12,7 @@ namespace data {
 	public:
 		class DataReceiverException: public std::exception {
 		public:
-			DataReceiverException(const std::string& where, const std::string& what);
+			DataReceiverException(const std::string& where, const std::string& what, DataReceiver& receiver);
 			DataReceiverException(const DataReceiverException& other) = default;
 			DataReceiverException& operator=(const DataReceiverException& other) = default;
 			virtual const char* what() const noexcept override;
@@ -20,31 +20,39 @@ namespace data {
 			std::string m_msg;
 		};
 
-		DataReceiver(const std::string& header, const std::size_t& length_field_size, const std::size_t& max_data_size);
+		DataReceiver(std::vector<char>& header, const std::size_t& length_field_size, const std::size_t& max_data_size);
 
 		DataReceiver(const DataReceiver& other) = default;
 		DataReceiver& operator=(const DataReceiver& other) = default;
 
 		virtual void onEvent(char event) override;
+		void set_data_listener(common::IListener<const std::vector<char>&> *data_listener_ptr);
+		void reset_receiver();
 	private:
-		const std::string m_header;
+		const std::vector<char> m_header;
 		const std::size_t m_length_field_size;
 		const std::size_t m_max_data_size;
 
-		enum ReceiverState {
+		common::IListener<const std::vector<char>&> *m_data_listener_ptr;
+
+		enum class ReceiverState: int {
 			RECEIVING_HEADER,
 			RECEIVING_SIZE,
 			RECEIVING_DATA
 		};
 
 		ReceiverState m_state;
-		std::iostream m_buffer;
+		std::size_t m_data_size;
+		std::vector<char> m_data_buff;
+
+		void receive_header(char event);
+		void receive_size(char event);
+		void receive_data(char event);
+
+		std::size_t deserialize_data_length() const;
 
 		static std::size_t init_length_field_size(const std::size_t& length_field_size);
 		
-		static bool capture_header(std::istream& data_stream);
-
-		static std::size_t deserialize_data_length(const std::list<char>& serial_data_length);
 	}; // DataReceiver
 
 	inline const char *DataReceiver::DataReceiverException::what() const noexcept {
