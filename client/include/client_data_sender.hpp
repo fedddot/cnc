a9e9e1dfd14cc939a0bf28dea21224c2e7ce507d
idan.rom <idanrommm@gmail.com>
2023-08-12 04:38:18 +0300

    implemented

--- client/include/client_data_sender.hpp
+++ client/include/client_data_sender.hpp
@@ -2,6 +2,7 @@
 #define	__CLIENT_DATA_SENDER_HPP__
 
 #include <vector>
+#include <string>
 #include <cstddef>
 
 #include "data_sender.hpp"
@@ -9,8 +10,19 @@
 namespace communication {
 	class ClientDataSender: public DataSender {
 	public:
-		ClientDataSender(const std::vector<char>& header, const std::size_t& length_field_size);
+		ClientDataSender(const std::vector<char>& header, const std::size_t& length_field_size, const std::string& port_path);
 		virtual void send(const std::vector<char>& data) override;
+		virtual ~ClientDataSender() noexcept override;
+	private:
+		enum { BAD_FD = -1 };
+		const std::string m_port_path;
+		int m_port_fd;
+
+		static int open_port(const std::string& port_path);
+		static void config_port(int port_fd);
+		static void close_port(int port_fd);
+
+		std::vector<char> wrap_data(const std::vector<char>& data) const;
 	}; // ClientDataSender
 } // namespace communication
 #endif // __CLIENT_DATA_SENDER_HPP__
