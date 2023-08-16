43385e81c0eebefb03c7fc19d52bbbe88e681691
idan.rom <idanrommm@gmail.com>
2023-08-16 22:04:30 +0300

    pico_sdk set up

--- server/communication/src/server_sender.cpp
+++ server/communication/src/server_sender.cpp
@@ -2,6 +2,9 @@
 #include <vector>
 #include <cstddef>
 
+#include <stdio.h>
+#include "pico/stdlib.h"
+
 #include "idata.hpp"
 #include "iserializer.hpp"
 #include "json_serializer.hpp"
@@ -13,7 +16,7 @@ using namespace data;
 using namespace json;
 
 ServerDataSender::ServerDataSender(const std::vector<char>& header, const std::size_t& length_field_size): ByteSender(header, length_field_size, ByteSender::SerializerSmartPtr(new JsonSerializer)) {
-
+    setup_default_uart();
 }
 
 ServerDataSender::~ServerDataSender() noexcept {
@@ -22,5 +25,7 @@ ServerDataSender::~ServerDataSender() noexcept {
 
 void ServerDataSender::send(const IData& data) {
 	auto prepared_data = prepare_data(data);
-	
+	std::string str_data;
+	str_data.insert(str_data.end(), prepared_data.begin(), prepared_data.end());
+	fwrite(str_data.c_str(), 1, str_data.size(), stdout);
 }
