b721f29c607ce6a8fc06581891364bd578e00b8e
idan.rom <idanrommm@gmail.com>
2023-08-16 19:19:08 +0300

    updated

--- server/tasks/src/server_movement_task.cpp
+++ server/tasks/src/server_movement_task.cpp
@@ -4,14 +4,11 @@
 #include "idata.hpp"
 #include "object.hpp"
 #include "string.hpp"
-#include "isender.hpp"
 #include "movement_task.hpp"
-#include "server_sender.hpp"
 #include "server_movement_task.hpp"
 
 using namespace task;
 using namespace data;
-using namespace communication;
 
 ServerMovementTask::ServerMovementTask(const Distance& distance, const Speed& speed, const Axis& axis): MovementTask(distance, speed, axis), m_report(createReport("not executed")) {
 
