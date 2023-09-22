0b41258fa5ca4d7a94d6c07cf618c7383aa96ccb
idan.rom <idanrommm@gmail.com>
2023-09-22 16:58:51 +0300

    implemented

--- server/communication/include/server_package_manager.hpp
+++ server/communication/include/server_package_manager.hpp
@@ -3,15 +3,17 @@
 
 #include "package_manager.hpp"
 #include "package_descriptor.hpp"
+#include "server_uart.hpp"
 
 namespace communication {
 	class ServerPackageManager: public PackageManager {
 	public:
-		ServerPackageManager(const PackageDescriptor& package_descriptor);
+		ServerPackageManager(const PackageDescriptor& package_descriptor, hardware::ServerUart& uart);
 		ServerPackageManager(const ServerPackageManager& other) = delete;
 		ServerPackageManager& operator=(const ServerPackageManager& other) = delete;
+		virtual ~ServerPackageManager() noexcept override;
 	private:
-
+		hardware::ServerUart& m_uart;
 	}; // ServerPackageManager
 } // namespace communication
 #endif // __SERVER_PACKAGE_MANAGER_HPP__
--- server/communication/src/server_package_manager.cpp
+++ server/communication/src/server_package_manager.cpp
@@ -1,14 +1,18 @@
 #include <stdexcept>
 
-#include "uart.hpp"
 #include "package_manager.hpp"
 #include "package_descriptor.hpp"
+#include "server_uart.hpp"
 
 #include "server_package_manager.hpp"
 
 using namespace communication;
 using namespace hardware;
 
-ServerPackageManager::ServerPackageManager(const PackageDescriptor& package_descriptor): PackageManager(package_descriptor, AAA) {
-	
+ServerPackageManager::ServerPackageManager(const PackageDescriptor& package_descriptor, ServerUart& uart): PackageManager(package_descriptor, uart), m_uart(uart) {
+	m_uart.set_char_listener(&(receiver()));
+}
+
+ServerPackageManager::~ServerPackageManager() noexcept {
+	m_uart.set_char_listener(nullptr);
 }
