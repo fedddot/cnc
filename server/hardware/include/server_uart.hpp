1a98cad0720ade032be1356fc76bc200b20bf724
idan.rom <idanrommm@gmail.com>
2023-09-22 16:08:11 +0300

    renamed and reimplemented

---
+++ server/hardware/include/server_uart.hpp
@@ -0,0 +1,46 @@
+#ifndef	__SERVER_UART_HPP__
+#define	__SERVER_UART_HPP__
+
+#include <map>
+#include <vector>
+
+#include "ilistener.hpp"
+#include "uart.hpp"
+
+namespace communication {
+	class ServerUart: public Uart {
+	public:
+		enum class UartId: int {
+			UART0,
+			UART1
+		};
+		ServerUart(BaudRate baud_rate, Parity parity, StopBits stop_bits, BitsNumber bits_number, UartId uart_id);
+		ServerUart(const ServerUart& other) = delete;
+		ServerUart& operator=(const ServerUart& other) = delete;
+
+		~ServerUart() noexcept override;
+		virtual void send(const std::vector<char>& data) override;
+
+		inline UartId uart_id() const;
+	private:
+		UartId m_uart_id;
+
+		
+
+		void init_gpios();
+		void init();
+		void init_data_format();
+		void init_interrupts();
+
+		static std::map<UartId, ServerUart *> s_interrupts_mapping;
+
+		static common::IListener<char> *getCharListener(const UartId& id);
+		static void on_uart0_rx();
+		static void on_uart1_rx();
+	}; // ServerUart
+
+	inline ServerUart::UartId ServerUart::uart_id() const {
+		return m_uart_id;
+	}
+} // namespace communication
+#endif // __SERVER_UART_HPP__
