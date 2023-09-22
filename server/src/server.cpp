4f1fa9f534b3c587a4084e844d537c1a096741db
idan.rom <idanrommm@gmail.com>
2023-09-22 17:40:32 +0300

    reimplementation started

--- server/src/server.cpp
+++ server/src/server.cpp
@@ -1,59 +1,26 @@
-#include <cstddef>
+#include <string>
 #include <vector>
-#include <memory>
-#include <algorithm>
 
-#include <stdio.h>
-#include "hardware/rtc.h"
-#include "pico/stdlib.h"
-#include "pico/util/datetime.h"
+#include "server_uart.hpp"
+#include "package_manager.hpp"
+#include "package_descriptor.hpp"
 
-#include "server_sender.hpp"
-#include "server_receiver.hpp"
-#include "server_task_manager.hpp"
-
-#include "idata.hpp"
-#include "string.hpp"
-#include "object.hpp"
-
-#include "uart_handler.hpp"
-
-using namespace task;
-using namespace data;
+using namespace hardware;
 using namespace communication;
 
-static inline std::vector<char> strToVector(const std::string& str) {
-	std::vector<char> result;
-	result.insert(result.end(), str.begin(), str.end());
-	return result;
-}
-
 int main(void) {
-	const std::vector<char> header(strToVector("msg_header"));
-	const std::size_t length_field_size(2UL);
-	const std::size_t length_max(200UL);
+	const std::string header_str("cnc_server");
+	const std::size_t package_length_field_size(2UL);
+	ServerUart uart(
+		ServerUart::BaudRate::B115200, 
+		ServerUart::Parity::NONE, 
+		ServerUart::StopBits::ONE, 
+		ServerUart::BitsNumber::BN8, 
+		ServerUart::UartId::UART0
+	);
 
-	UartHandler uart_handler(115200);
-	
-	ServerTaskManager task_manager(ServerTaskManager::ServerSenderSmartPtr(new ServerDataSender(header, length_field_size, uart_handler)), "type");
-
-	ServerDataReceiver receiver(header, length_field_size, length_max);
-	uart_handler.set_byte_listener(&receiver);
-
-	receiver.set_data_listener(&task_manager);
-
-	uart_handler.start();
-
-	while (true) {
-		if (!task_manager.is_task_pending()) {
-			continue;
-		}
-		auto task_ptr = task_manager.dequeue_task();
-		task_ptr->execute();
-		auto task_report = task_ptr->report();
-		task_manager.accessSender().send(*task_report);
-		// sleep_ms(1000);
-	}	
-
-	return 0;
+	PackageDescriptor package_descriptor(
+		std::vector<char>({'c'})
+	);
+	PackageManager package_manager()
 }
