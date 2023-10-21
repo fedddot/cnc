db2ef81a87ce7bc493e84e0a333d2bad5207635b
idan.rom <idanrommm@gmail.com>
2023-10-21 09:40:00 +0300

    steps_task_creator implemented

--- server/tasks/include/steps_task_creator.hpp
+++ server/tasks/include/steps_task_creator.hpp
@@ -1,38 +1,32 @@
-#ifndef	__REGISTER_STEPPER_MOTOR_TASK_CREATOR_HPP__
-#define	__REGISTER_STEPPER_MOTOR_TASK_CREATOR_HPP__
+#ifndef	__STEPS_TASK_CREATOR_HPP__
+#define	__STEPS_TASK_CREATOR_HPP__
 
 #include <string>
-#include <map>
 #include <memory>
 
+#include "stepper_motor.hpp"
 #include "object.hpp"
 #include "server_task.hpp"
 #include "registry.hpp"
-#include "gpio.hpp"
-#include "register_stepper_motor_task.hpp"
 #include "icreator.hpp"
 
 namespace task {
-	class RegisterStepperMotorTaskCreator: public common::ICreator<std::shared_ptr<ServerTask>, data::Object> {
+	class StepsTaskCreator: public common::ICreator<std::shared_ptr<ServerTask>, data::Object> {
 	public:
-		typedef RegisterStepperMotorTask::StepperMotorRegistry StepperMotorRegistry;
-		typedef common::Registry<hardware::Gpio::PinNumber, std::shared_ptr<hardware::Gpio>> GpioRegistry;
-		typedef std::map<hardware::StepperMotor::BridgeShoulder, std::string> ControlGpiosFieldNamesMapping;
+		typedef common::Registry<std::string, std::shared_ptr<hardware::StepperMotor>> StepperMotorRegistry;
 		
-		RegisterStepperMotorTaskCreator(StepperMotorRegistry& stepper_motors_registry, GpioRegistry& gpios_registry, const ControlGpiosFieldNamesMapping& gpios_field_names_mapping, const std::string& id_field = "id", const std::string& motor_id_field = "motor_id", const std::string& control_gpios_field = "control_gpios");
-		RegisterStepperMotorTaskCreator(const RegisterStepperMotorTaskCreator& other) = delete;
-		RegisterStepperMotorTaskCreator& operator=(const RegisterStepperMotorTaskCreator& other) = delete;
+		StepsTaskCreator(StepperMotorRegistry& stepper_motors_registry, const std::string& id_field = "id", const std::string& motor_id_field = "motor_id", const std::string& direction_field = "direction", const std::string& steps_number_field = "steps_number", const std::string& step_duration_ms_field = "step_duration_ms");
+		StepsTaskCreator(const StepsTaskCreator& other) = delete;
+		StepsTaskCreator& operator=(const StepsTaskCreator& other) = delete;
 
 		virtual std::shared_ptr<ServerTask> create(const data::Object& config_data) override;
 	private:
 		StepperMotorRegistry& m_stepper_motors_registry;
-		GpioRegistry& m_gpios_registry;
-		ControlGpiosFieldNamesMapping m_gpios_field_names_mapping;
-		std::string m_id_field;
-		std::string m_motor_id_field;
-		std::string m_control_gpios_field;
-
-		hardware::StepperMotor::ControlGpios init_control_gpios(const data::Object& config_data);
-	}; // RegisterStepperMotorTaskCreator
+		const std::string m_id_field;
+		const std::string m_motor_id_field;
+		const std::string m_direction_field;
+		const std::string m_steps_number_field;
+		const std::string m_step_duration_ms_field;
+	}; // StepsTaskCreator
 } // namespace task
-#endif // __REGISTER_STEPPER_MOTOR_TASK_CREATOR_HPP__
+#endif // __STEPS_TASK_CREATOR_HPP__
--- server/tasks/src/steps_task_creator.cpp
+++ server/tasks/src/steps_task_creator.cpp
@@ -2,19 +2,22 @@
 #include <string>
 #include <memory>
 
+#include "steps_task.hpp"
+#include "stepper_motor.hpp"
 #include "string.hpp"
 #include "object.hpp"
 #include "server_task.hpp"
-#include "register_gpio_task.hpp"
+#include "registry.hpp"
 #include "icreator.hpp"
 
-#include "register_gpio_task_creator.hpp"
+#include "steps_task_creator.hpp"
 
 using namespace task;
 using namespace data;
 using namespace common;
+using namespace hardware;
 
-RegisterGpioTaskCreator::RegisterGpioTaskCreator(RegisterGpioTask::GpioRegistry& registry, const std::string& id_field, const std::string& pin_number_field, const std::string& direction_field): m_registry(registry), m_id_field(id_field), m_pin_number_field(pin_number_field), m_direction_field(direction_field) {
+StepsTaskCreator::StepsTaskCreator(StepperMotorRegistry& stepper_motors_registry, const std::string& id_field, const std::string& motor_id_field, const std::string& direction_field, const std::string& steps_number_field, const std::string& step_duration_ms_field): m_stepper_motors_registry(stepper_motors_registry), m_id_field(id_field), m_motor_id_field(motor_id_field), m_direction_field(direction_field), m_steps_number_field(steps_number_field), m_step_duration_ms_field(step_duration_ms_field) {
 
 }
 
@@ -30,24 +33,26 @@ T getField(const Object& data_object, const std::string& field_name) {
 	return T(*(iter->second));
 }
 
-RegisterGpioTask::Direction getDirection(const String& str_data) {
-	if ("IN" == str_data) {
-		return RegisterGpioTask::Direction::IN;
+static StepperMotor::Direction strToDirection(const String& str) {
+	if ("CW" == str) {
+		return StepperMotor::Direction::CW;
 	}
-	if ("OUT" == str_data) {
-		return RegisterGpioTask::Direction::OUT;
+	if ("CCW" == str) {
+		return StepperMotor::Direction::CCW;
 	}
-	throw std::invalid_argument("failed to cast direction from str data");
+	throw std::invalid_argument("unsupported direction string received (\"" + str + "\")");
 }
 
-RegisterGpioTask::PinNumber getPinNumber(const String& str_data) {
-	return static_cast<RegisterGpioTask::PinNumber>(std::atoi(str_data.c_str()));
-}
+std::shared_ptr<ServerTask> StepsTaskCreator::create(const Object& config_data) {
+	String task_id(getField<String>(config_data, m_id_field));
+	String motor_id(getField<String>(config_data, m_motor_id_field));
 
-std::shared_ptr<ServerTask> RegisterGpioTaskCreator::create(const Object& config_data) {
-	String task_id = getField<String>(config_data, m_id_field);
-	String pin_number_str = getField<String>(config_data, m_pin_number_field);
-	String direction_str = getField<String>(config_data, m_direction_field);
+	StepperMotor& stepper_motor = *(m_stepper_motors_registry.access_member(motor_id));
 
-	return std::shared_ptr<ServerTask>(new RegisterGpioTask(task_id, getPinNumber(pin_number_str), getDirection(direction_str), m_registry));
+	String direction_str(getField<String>(config_data, m_direction_field));
+	String steps_number_str(getField<String>(config_data, m_steps_number_field));
+	String step_duration_ms_str(getField<String>(config_data, m_step_duration_ms_field));
+
+
+	return std::shared_ptr<ServerTask>(new StepsTask(task_id, stepper_motor, strToDirection(direction_str), std::atoi(steps_number_str.c_str()), std::atoi(step_duration_ms_str.c_str())));
 }
