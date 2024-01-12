58b0e2e89882a280ff236e81505af7382e0e4ca6
idan.rom <idanrommm@gmail.com>
2024-01-13 00:55:38 +0200

    report class implemented and applied

--- server/cnc_engine/include/engine.hpp
+++ server/cnc_engine/include/engine.hpp
@@ -5,14 +5,15 @@
 
 #include "factory.hpp"
 #include "data.hpp"
+#include "report.hpp"
 #include "sender.hpp"
 #include "task.hpp"
 
 namespace cnc_engine {
 	class Engine {
 	public:
-		using TaskFactory = Factory<Task<Data> *, Data>;
-		using ReportSender = Sender<Data>;
+		using TaskFactory = Factory<Task<Report> *, Data>;
+		using ReportSender = Sender<Report>;
 
 		inline Engine(TaskFactory& task_factory, ReportSender& report_sender);
 		Engine(const Engine& other) = delete;
@@ -30,8 +31,8 @@ namespace cnc_engine {
 	}
 
 	void Engine::run_task(const Data& task_cfg_data) {
-		std::unique_ptr<Task<Data>> task_ptr(m_task_factory.create(task_cfg_data));
-		Treport report = task_ptr->execute();
+		std::unique_ptr<Task<Report>> task_ptr(m_task_factory.create(task_cfg_data));
+		Report report = task_ptr->execute();
 		m_report_sender.send(report);
 	}
 }
