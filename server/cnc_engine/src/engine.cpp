5134a38d5e7f5d817b1c2c6ab1afae9625724e77
idan.rom <idanrommm@gmail.com>
2024-01-14 21:56:09 +0200

    engine implemented in cpp

---
+++ server/cnc_engine/src/engine.cpp
@@ -0,0 +1,17 @@
+#include "data.hpp"
+#include "report.hpp"
+#include "task.hpp"
+
+#include "engine.hpp"
+
+using namespace cnc_engine;
+
+Engine::Engine(TaskFactory& task_factory, ReportSender& report_sender): m_task_factory(task_factory), m_report_sender(report_sender) {
+
+}
+
+void Engine::run_task(const Data& task_cfg_data) {
+	std::unique_ptr<Task<Report>> task_ptr(m_task_factory.create(task_cfg_data));
+	Report report = task_ptr->execute();
+	m_report_sender.send(report);
+}
