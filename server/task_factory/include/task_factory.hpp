80b55960d653d9612cfb9266d1fe7a160f2b881b
idan.rom <idanrommm@gmail.com>
2024-01-17 21:06:49 +0200

    partially implemented

--- server/task_factory/include/task_factory.hpp
+++ server/task_factory/include/task_factory.hpp
@@ -1,22 +1,42 @@
 #ifndef	TASK_FACTORY_HPP
 #define	TASK_FACTORY_HPP
 
+#include <map>
+#include <memory>
+
 #include "factory.hpp"
 #include "report.hpp"
 #include "task.hpp"
 #include "data.hpp"
+#include "creator.hpp"
 
 namespace task_factory {
 	using Task = cnc_engine::Task<cnc_engine::Report>;
 	using TaskCfg = cnc_engine::Data;
 	using Report = cnc_engine::Report;
 
+	class Gpio;
+	class StepperMotor;
+
 	class TaskFactory: public cnc_engine::Factory<Task *, TaskCfg> {
 	public:
 		TaskFactory();
 		TaskFactory(const TaskFactory& other) = default;
 		TaskFactory& operator=(const TaskFactory& other) = default;
+
 		virtual Task *create(const TaskCfg& cfg) const override;
+
+	private:
+		enum class TaskType: int {
+			INIT_GPIO,
+			INIT_STEP_MOTOR,
+			MOVE
+		};
+		std::map<TaskType, std::unique_ptr<Creator<Task *, TaskCfg>>> m_creators;
+
+		static const std::string s_task_type_field_name;
+
+		static TaskType get_task_type(const TaskCfg& cfg);
 	};
 }
 #endif // TASK_FACTORY_HPP
--- server/task_factory/src/task_factory.cpp
+++ server/task_factory/src/task_factory.cpp
@@ -1,12 +1,47 @@
+#include <map>
+#include <stdexcept>
+
+#include "integer.hpp"
+#include "object.hpp"
+#include "data.hpp"
+#include "creator.hpp"
 #include "task_factory.hpp"
-#include <cstddef>
 
 using namespace task_factory;
 
-TaskFactory::TaskFactory() {
+const std::string task_factory::TaskFactory::s_task_type_field_name("task_type");
+
+
+TaskFactory::TaskFactory(): m_creators({
+	{TaskType::INIT_GPIO, nullptr}, 
+	{TaskType::INIT_STEP_MOTOR, nullptr}, 
+	{TaskType::MOVE, nullptr}
+}) {
 
 }
 
+TaskFactory::TaskType TaskFactory::get_task_type(const TaskCfg& cfg) {
+	const cnc_engine::Object& cfg_object = TaskCfg::cast<cnc_engine::Object>(cfg);
+	if (!cfg_object.contains(s_task_type_field_name)) {
+		throw std::invalid_argument("received cfg object doesn't contain field " + s_task_type_field_name);
+	}
+	const cnc_engine::Integer& int_type = TaskCfg::cast<cnc_engine::Integer>(cfg_object.access(s_task_type_field_name));
+	
+	switch (int_type.get()) {
+	case static_cast<int>(TaskType::INIT_GPIO):
+		return TaskType::INIT_GPIO;
+	case static_cast<int>(TaskType::INIT_STEP_MOTOR):
+		return TaskType::INIT_STEP_MOTOR;
+	case static_cast<int>(TaskType::MOVE):
+		return TaskType::MOVE;
+	}
+	throw std::invalid_argument("failed to retrieve task type from the config");
+}
+
 Task *TaskFactory::create(const TaskCfg& cfg) const {
-	return nullptr;
+	auto iter = m_creators.find(get_task_type(cfg));
+	if (m_creators.end() == iter) {
+		throw std::invalid_argument("creator matching received cfg is not registered");
+	}
+	return iter->second->create(cfg);
 }
