1302b5cd21be2b91bada822d63286abc842fd74a
idan.rom <idanrommm@gmail.com>
2024-01-18 21:50:09 +0200

    tests added

---
+++ server/inventory/tests/src/ut_inventory.cpp
@@ -0,0 +1,25 @@
+#include "gtest/gtest.h"
+
+#include <stdexcept>
+#include <string>
+#include "inventory.hpp"
+
+using namespace inventory;
+
+TEST(ut_inventory, sanity) {
+	// WHEN
+	Inventory<int, std::string> instance;
+	std::string *item_ptr = nullptr;
+
+	// THEN
+	ASSERT_NO_THROW(instance.put(0, new std::string("0")));
+	ASSERT_THROW(instance.put(0, new std::string("1")), std::invalid_argument);
+	ASSERT_TRUE(instance.contains(0));
+	ASSERT_NO_THROW(item_ptr = instance.get(0));
+	ASSERT_FALSE(instance.contains(0));
+	ASSERT_THROW(instance.get(0), std::invalid_argument);
+	ASSERT_THROW(instance.put(1, nullptr), std::invalid_argument);
+
+	// CLEANUP
+	delete item_ptr;
+}
