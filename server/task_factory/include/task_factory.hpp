18c8a49a422c914b9eb2b00537d5b83ab5683285
idan.rom <idanrommm@gmail.com>
2024-01-18 19:35:39 +0200

    implemented

--- server/task_factory/include/task_factory.hpp
+++ server/task_factory/include/task_factory.hpp
@@ -2,7 +2,6 @@
 #define	TASK_FACTORY_HPP
 
 #include <map>
-#include <memory>
 
 #include "factory.hpp"
 #include "report.hpp"
@@ -11,32 +10,25 @@
 #include "creator.hpp"
 
 namespace task_factory {
-	using Task = cnc_engine::Task<cnc_engine::Report>;
-	using TaskCfg = cnc_engine::Data;
-	using Report = cnc_engine::Report;
-
-	class Gpio;
-	class StepperMotor;
-
-	class TaskFactory: public cnc_engine::Factory<Task *, TaskCfg> {
+	class TaskFactory: public cnc_engine::Factory<cnc_engine::Task<cnc_engine::Report> *, cnc_engine::Data> {
 	public:
-		TaskFactory();
+		enum class TaskType: int {
+			CREATE_INVENTORY_ITEM,
+			DELETE_INVENTORY_ITEM
+		};
+		using Task = cnc_engine::Task<cnc_engine::Report>;
+		using TaskConfigData = cnc_engine::Data;
+		using TaskCreator = Creator<Task *, TaskConfigData>;
+
+		TaskFactory(const std::map<TaskType, TaskCreator&>& creators);
 		TaskFactory(const TaskFactory& other) = default;
 		TaskFactory& operator=(const TaskFactory& other) = default;
 
-		virtual Task *create(const TaskCfg& cfg) const override;
-
+		virtual Task *create(const TaskConfigData& cfg) const override;
 	private:
-		enum class TaskType: int {
-			INIT_GPIO,
-			INIT_STEP_MOTOR,
-			MOVE
-		};
-		std::map<TaskType, std::unique_ptr<Creator<Task *, TaskCfg>>> m_creators;
-
+		std::map<TaskType, TaskCreator&> m_creators;
 		static const std::string s_task_type_field_name;
-
-		static TaskType get_task_type(const TaskCfg& cfg);
+		static TaskType retrieve_task_type(const TaskConfigData& cfg);
 	};
 }
 #endif // TASK_FACTORY_HPP
--- server/task_factory/src/task_factory.cpp
+++ server/task_factory/src/task_factory.cpp
@@ -3,45 +3,39 @@
 
 #include "integer.hpp"
 #include "object.hpp"
-#include "data.hpp"
 #include "creator.hpp"
 #include "task_factory.hpp"
 
 using namespace task_factory;
+using namespace cnc_engine;
 
 const std::string task_factory::TaskFactory::s_task_type_field_name("task_type");
 
 
-TaskFactory::TaskFactory(): m_creators({
-	{TaskType::INIT_GPIO, nullptr}, 
-	{TaskType::INIT_STEP_MOTOR, nullptr}, 
-	{TaskType::MOVE, nullptr}
-}) {
+TaskFactory::TaskFactory(const std::map<TaskType, TaskCreator&>& creators): m_creators(creators) {
 
 }
 
-TaskFactory::TaskType TaskFactory::get_task_type(const TaskCfg& cfg) {
-	const cnc_engine::Object& cfg_object = TaskCfg::cast<cnc_engine::Object>(cfg);
+TaskFactory::TaskType TaskFactory::retrieve_task_type(const TaskConfigData& cfg) {
+	const Object& cfg_object = TaskConfigData::cast<Object>(cfg);
 	if (!cfg_object.contains(s_task_type_field_name)) {
 		throw std::invalid_argument("received cfg object doesn't contain field " + s_task_type_field_name);
 	}
-	const cnc_engine::Integer& int_type = TaskCfg::cast<cnc_engine::Integer>(cfg_object.access(s_task_type_field_name));
+	const Integer& int_type = TaskConfigData::cast<Integer>(cfg_object.access(s_task_type_field_name));
 	
 	switch (int_type.get()) {
-	case static_cast<int>(TaskType::INIT_GPIO):
-		return TaskType::INIT_GPIO;
-	case static_cast<int>(TaskType::INIT_STEP_MOTOR):
-		return TaskType::INIT_STEP_MOTOR;
-	case static_cast<int>(TaskType::MOVE):
-		return TaskType::MOVE;
+	case static_cast<int>(TaskType::CREATE_INVENTORY_ITEM):
+		return TaskType::CREATE_INVENTORY_ITEM;
+	case static_cast<int>(TaskType::DELETE_INVENTORY_ITEM):
+		return TaskType::DELETE_INVENTORY_ITEM;
 	}
 	throw std::invalid_argument("failed to retrieve task type from the config");
 }
 
-Task *TaskFactory::create(const TaskCfg& cfg) const {
-	auto iter = m_creators.find(get_task_type(cfg));
+TaskFactory::Task *TaskFactory::create(const TaskConfigData& cfg) const {
+	auto iter = m_creators.find(retrieve_task_type(cfg));
 	if (m_creators.end() == iter) {
 		throw std::invalid_argument("creator matching received cfg is not registered");
 	}
-	return iter->second->create(cfg);
+	return (iter->second).create(cfg);
 }
