9b08bb4e2c787640b9af24fef2cce48469fdafd9
idan.rom <idanrommm@gmail.com>
2024-01-26 23:36:55 +0200

    updated

--- server/cnc_engine/src/engine.cpp
+++ server/cnc_engine/src/engine.cpp
@@ -1,6 +1,9 @@
 #include "data.hpp"
+#include "object.hpp"
 #include "report.hpp"
+#include "string.hpp"
 #include "task.hpp"
+#include <exception>
 
 #include "engine.hpp"
 
@@ -11,7 +14,14 @@ Engine::Engine(TaskFactory& task_factory, ReportSender& report_sender): m_task_f
 }
 
 void Engine::run_task(const Data& task_cfg_data) {
-	std::unique_ptr<basics::Task<Report>> task_ptr(m_task_factory.create(task_cfg_data));
-	Report report = task_ptr->execute();
-	m_report_sender.send(report);
+	std::unique_ptr<basics::Task> task_ptr(m_task_factory.create(task_cfg_data));
+	Report::Result report_result(Report::Result::SUCCESS);
+	Object report_data;
+	try {
+		task_ptr->execute();
+	} catch (const std::exception& e) {
+		report_data.add("exception", String(e.what()));
+		report_result = Report::Result::FAILURE;
+	}
+	m_report_sender.send(Report(report_result, report_data));
 }
