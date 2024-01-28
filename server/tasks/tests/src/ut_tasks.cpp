de3685180f65d5e603d3cef63e63589fe20552f2
fedddot <idanrommm@gmail.com>
2024-01-28 20:30:08 +0200

    implemented

--- server/tasks/tests/src/ut_tasks.cpp
+++ server/tasks/tests/src/ut_tasks.cpp
@@ -6,11 +6,39 @@
 #include "delete_inventory_item_task.hpp"
 #include "integer.hpp"
 #include "inventory.hpp"
+#include "inventory_task.hpp"
+#include "object.hpp"
 
 using namespace basics;
 using namespace inventory;
 using namespace tasks;
 using namespace data;
+
+TEST(ut_tasks, InventoryTask_sanity) {
+	// GIVEN
+	Inventory<int, int> test_inventory;
+	test_inventory.put(0, std::shared_ptr<int>(new int(10)));
+	test_inventory.put(1, std::shared_ptr<int>(new int(20)));
+	Object task_data;
+	task_data.add("id", Integer(1));
+	task_data.add("val", Integer(30));
+
+	// WHEN
+	InventoryTask<int, int> instance(
+		&test_inventory,
+		[&](Inventory<int, int> *inventory, const Data& data)-> void {
+			const Object& data_object = Data::cast<Object>(data);
+			auto id = Data::cast<Integer>(data_object.access("id")).get();
+			auto val = Data::cast<Integer>(data_object.access("val")).get();
+			*(inventory->get(id)) = val;
+		},
+		task_data
+	);
+	
+	// THEN
+	ASSERT_NO_THROW(instance.execute());
+	ASSERT_EQ(30, *(test_inventory.get(1)));
+}
 
 TEST(ut_tasks, CreateInventoryItemTask_sanity) {
 	// GIVEN
