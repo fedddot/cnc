50c4e554ced41f8523153d5b81fd08d773690be5
fedddot <idanrommm@gmail.com>
2024-02-03 12:03:56 +0200

    report getter added

--- server/engine/include/task.hpp
+++ server/engine/include/task.hpp
@@ -1,11 +1,13 @@
 #ifndef	TASK_HPP
 #define	TASK_HPP
 
+#include "data.hpp"
 namespace basics {
 	class Task {
 	public:
 		virtual ~Task() noexcept = default;
 		virtual void execute() = 0;
+		virtual data::Data *report() const = 0;
 	};
 }
 
