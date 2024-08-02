53cf2fa45ede231156ac949cbc71b5e2b6d0db9d
fedddot <idanrommm@gmail.com>
2024-08-02 22:08:21 +0300

    untemplated

--- include/stepper_motor_states.hpp
+++ include/stepper_motor_states.hpp
@@ -8,52 +8,47 @@
 
 namespace cnc {
 
-	template <typename Tgpio_id>
 	class StepperMotorStates {
 	public:
-		StepperMotorStates(const std::vector<StepperMotorState<Tgpio_id>>& states);
+		StepperMotorStates(const std::vector<StepperMotorState>& states);
 		StepperMotorStates(const StepperMotorStates& other) = default;
 		StepperMotorStates& operator=(const StepperMotorStates& other) = default;
 		virtual ~StepperMotorStates() noexcept = default;
 
 		void move_back();
 		void move_forward();
-		StepperMotorState<Tgpio_id> current() const;
+		StepperMotorState current() const;
 	private:
-		std::vector<StepperMotorState<Tgpio_id>> m_states;
+		std::vector<StepperMotorState> m_states;
 		
-		using StateIter = typename std::vector<StepperMotorState<Tgpio_id>>::iterator;
+		using StateIter = typename std::vector<StepperMotorState>::iterator;
 		StateIter m_current_iter;
 	};
-
-	template <typename Tgpio_id>
-	inline StepperMotorStates<Tgpio_id>::StepperMotorStates(const std::vector<StepperMotorState<Tgpio_id>>& states): m_states(states) {
+	
+	inline StepperMotorStates::StepperMotorStates(const std::vector<StepperMotorState>& states): m_states(states) {
 		if (1 > m_states.size()) {
 			throw std::invalid_argument("states must contain at list 1 state");
 		}
 		m_current_iter = m_states.begin();
 	}
 
-	template <typename Tgpio_id>
-	inline void StepperMotorStates<Tgpio_id>::move_back() {
+	inline void StepperMotorStates::move_back() {
 		if (m_states.begin() == m_current_iter) {
 			m_current_iter = m_states.begin() + m_states.size() - 1;
 			return;
 		}
 		m_current_iter = m_current_iter - 1;
 	}
-
-	template <typename Tgpio_id>
-	inline void StepperMotorStates<Tgpio_id>::move_forward() {
+	
+	inline void StepperMotorStates::move_forward() {
 		if (m_states.end() == m_current_iter + 1) {
 			m_current_iter = m_states.begin();
 			return;
 		}
 		m_current_iter = m_current_iter + 1;
 	}
-
-	template <typename Tgpio_id>
-	inline StepperMotorState<Tgpio_id> StepperMotorStates<Tgpio_id>::current() const {
+	
+	inline StepperMotorState StepperMotorStates::current() const {
 		return *m_current_iter;
 	}
 }
