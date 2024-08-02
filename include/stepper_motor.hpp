0f971ee440808704ae46b1f80762e9e31ebd0b02
fedddot <idanrommm@gmail.com>
2024-08-02 22:46:55 +0300

    ctor implemented

--- include/stepper_motor.hpp
+++ include/stepper_motor.hpp
@@ -5,6 +5,7 @@
 #include <memory>
 #include <stdexcept>
 
+#include "array.hpp"
 #include "object.hpp"
 #include "stepper_motor_state.hpp"
 #include "stepper_motor_states.hpp"
@@ -44,6 +45,10 @@ namespace cnc {
 		StepperMotorStates m_states;
 		std::unique_ptr<MotorDataGenerator> m_data_generator;
 		std::unique_ptr<MotorTaskExecutor> m_executor;
+
+		TaskData create_gpios_data() const;
+		TaskData delete_gpios_data() const;
+		TaskData apply_state_data(const StepperMotorState& state) const;
 	};
 
 	template <typename Tgpio_id>
@@ -53,11 +58,26 @@ namespace cnc {
 		const MotorDataGenerator& data_generator,
 		const MotorTaskExecutor& executor
 	): m_shoulders(shoulders), m_states(states), m_data_generator(data_generator.clone()), m_executor(executor.clone()) {
-
+		using GpioState = typename StepperMotorState::GpioState;
+		m_executor->execute(create_gpios_data());
+		const StepperMotorState shutdown_state(
+			{
+				{Shoulder::LL, GpioState::LOW},
+				{Shoulder::LR, GpioState::LOW},
+				{Shoulder::HL, GpioState::LOW},
+				{Shoulder::HR, GpioState::LOW},
+			}
+		);
+		m_executor->execute(apply_state_data(shutdown_state));
 	}
 
 	template <typename Tgpio_id>
 	inline void StepperMotor<Tgpio_id>::steps(const Direction& direction, unsigned int steps_num, unsigned int step_duration_ms) const {
+		using namespace mcu_server;
+		Array tasks;
+		for (auto step_index = 0; steps_num > step_index; ++step_index) {
+			tasks.push_back(const Data &data)
+		}
 		throw std::runtime_error("NOT IMPLEMENTED");
 	}
 }
