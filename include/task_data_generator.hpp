078b10de7115c1b2172c1c64608ff62fb2faba14
fedddot <idanrommm@gmail.com>
2024-08-03 17:25:34 +0300

    methods added

--- include/task_data_generator.hpp
+++ include/task_data_generator.hpp
@@ -2,10 +2,11 @@
 #define	TASK_DATA_GENERATOR_HPP
 
 #include "array.hpp"
+#include "data.hpp"
 #include "gpio.hpp"
 
 namespace cnc {
-	template <typename Tdata, typename Tgpio_id>
+	template <typename Tdata, typename Tgpio_id, typename Ttask_id>
 	class TaskDataGenerator {
 	public:
 		using GpioDirection = typename mcu_platform::Gpio::Direction;
@@ -18,6 +19,7 @@ namespace cnc {
 		virtual Tdata generate_set_gpio_data(const Tgpio_id& id, const GpioState& state) const = 0;
 		virtual Tdata generate_delay_data(const unsigned int delay_ms) const = 0;
 		virtual Tdata generate_tasks_data(const mcu_server::Array& tasks) const = 0;
+		virtual Tdata generate_create_persistent_task_data(const Ttask_id& id, const mcu_server::Data& task_data) const = 0;
 		virtual TaskDataGenerator *clone() const = 0;
 	};
 }
