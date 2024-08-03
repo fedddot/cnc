9e5618cf82f2408edf54971068681281ca9652e6
fedddot <idanrommm@gmail.com>
2024-08-03 19:05:00 +0300

    test added

---
+++ tests/src/ut_persistent_task.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+#include "gtest/gtest.h"
+
+#include "object.hpp"
+
+using namespace mcu_server;
+
+TEST(ut_persistent_task, ctor_dtor_sanity) {
+	// GIVEN
+	Object task_data;
+	// WHEN
+	
+	// THEN
+}
