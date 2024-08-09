712490ebab6fb1d047cac6be1278c3b3115abbca
fedddot <idanrommm@gmail.com>
2024-08-09 15:25:34 +0300

    clone fixed

--- include/custom_persistent_task_allocator.hpp
+++ include/custom_persistent_task_allocator.hpp
@@ -16,7 +16,7 @@ namespace cnc {
 		mcu_server::Object create_data() const override;
 		mcu_server::Object delete_data() const override;
 		int id() const override;
-		PersistentTaskAllocator<mcu_server::Object, int> *clone() const override;
+		ServerObjectAllocator<mcu_server::Object> *clone() const override;
 	private:
 		int m_task_id;
 		mcu_server::Object m_task_data;
@@ -49,8 +49,8 @@ namespace cnc {
 		return m_task_id;
 	}
 
-	inline PersistentTaskAllocator<mcu_server::Object, int> *CustomPersistentTaskAllocator::clone() const {
-		return new CustomPersistentTask(*this);
+	inline ServerObjectAllocator<mcu_server::Object> *CustomPersistentTaskAllocator::clone() const {
+		return new CustomPersistentTaskAllocator(*this);
 	}
 }
 
