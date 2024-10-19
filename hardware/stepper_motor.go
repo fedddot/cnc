b692b6a278917eaaf37755a346db64843d4628f7
fedddot <idanrommm@gmail.com>
2024-10-19 17:05:55 +0000

    saved

--- hardware/stepper_motor.go
+++ hardware/stepper_motor.go
@@ -1,12 +1,5 @@
 package hardware
 
-type MotorDirection int
-
-const (
-	CW  MotorDirection = 0
-	CCW MotorDirection = 1
-)
-
 type StepperMotor interface {
 	Steps(steps_num uint, direction MotorDirection, time_on uint, time_off uint) error
 }
