a427adb9486dd6cc10c8566a5c3819cc8d5a259c
fedddot <idanrommm@gmail.com>
2024-10-19 16:04:51 +0000

    updated

--- model/axes.go
+++ model/axes.go
@@ -1,7 +1,5 @@
 package model
 
-import "fmt"
-
 type MovementExecutor interface {
 	Execute(movement Vector, feed float32) error
 }
@@ -12,31 +10,17 @@ type Axes struct {
 }
 
 func (i *Axes) Init(position Vector, executor MovementExecutor) {
-	for _, dim := range []Dimension{X, Y, Z} {
-		i.current_position[dim] = float32(0.0)
-		coord, ok := position[dim]
-		if ok {
-			i.current_position[dim] = coord
-		}
-	}
+	i.current_position = position
 	i.executor = executor
 }
 
 func (i *Axes) Move(movement Vector, feed float32) error {
-	updated_position := Vector{}
-	for _, dim := range []Dimension{X, Y, Z} {
-		delta_coord, ok := movement[dim]
-		if !ok {
-			return fmt.Errorf("failed to retrieve one of required dimensions")
-		}
-		updated_position[dim] = i.current_position[dim] + delta_coord
-	}
 	err := i.executor.Execute(movement, feed)
 	if err != nil {
 		return err
 	}
-	i.current_position = updated_position
-	return fmt.Errorf("NOT IMPLEMENTED")
+	i.current_position = i.current_position.Add(movement)
+	return nil
 }
 
 func (i *Axes) Position() Vector {
