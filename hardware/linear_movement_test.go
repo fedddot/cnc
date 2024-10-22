89c3f84d13c170a0cb70bd9c1e7396519d44fe39
fedddot <idanrommm@gmail.com>
2024-10-22 14:37:10 +0000

    test updated

--- hardware/linear_movement_test.go
+++ hardware/linear_movement_test.go
@@ -3,10 +3,34 @@ package hardware
 import (
 	"cnc/client/communication"
 	"cnc/client/model"
+	"math"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
+
+func createMovementBetweenSectors(phi1, phi2, radius float64) model.Vector[float32] {
+	result := model.Vector[float32]{}
+	result.Init(
+		float32(radius*(math.Cos(phi2)-math.Cos(phi1))),
+		float32(radius*(math.Sin(phi2)-math.Sin(phi1))),
+		0,
+	)
+	return result
+}
+
+func createChordMovements(radius float32, number_of_sectors uint) []model.Vector[float32] {
+	res := make([]model.Vector[float32], 0)
+	for sector_index := uint(1); sector_index < number_of_sectors; sector_index++ {
+		sector_movement := createMovementBetweenSectors(
+			2*math.Pi*float64(sector_index-1)/float64(number_of_sectors),
+			2*math.Pi*float64(sector_index)/float64(number_of_sectors),
+			float64(radius),
+		)
+		res = append(res, sector_movement)
+	}
+	return res
+}
 
 func TestMovement_Init_Uninit(t *testing.T) {
 	// GIVEN
@@ -20,56 +44,53 @@ func TestMovement_Init_Uninit(t *testing.T) {
 		model.X: {
 			Id: "X",
 			Config: StepperMotorGpoMapping{
-				A0: 10,
-				A1: 20,
-				B0: 30,
-				B1: 40,
-				En: 50,
+				A0: 16,
+				A1: 17,
+				B0: 18,
+				B1: 19,
+				En: 15,
 			},
 		},
 		model.Y: {
 			Id: "Y",
 			Config: StepperMotorGpoMapping{
-				A0: 60,
-				A1: 70,
-				B0: 80,
-				B1: 90,
-				En: 100,
+				A0: 6,
+				A1: 5,
+				B0: 4,
+				B1: 3,
+				En: 7,
 			},
 		},
 		model.Z: {
 			Id: "Z",
 			Config: StepperMotorGpoMapping{
-				A0: 110,
-				A1: 120,
-				B0: 130,
-				B1: 140,
-				En: 150,
+				A0: 13,
+				A1: 12,
+				B0: 11,
+				B1: 10,
+				En: 14,
 			},
 		},
 	}
 	steps_per_unit := uint(100)
 	time_divider := uint(1000000) // s -> us
-	test_movement := model.Vector[float32]{}
-	test_movement.Init(10.3, 12.43, 139.2)
-	test_feed := float32(15.4)
+
+	test_feed := float32(16)
 
 	// WHEN
-	// connection := communication.TestConnection{}
-	// connection.Init(
-	// func(request communication.Request) (communication.Response, error) {
-	// return communication.Response{}, nil
-	// },
-	// )
-	connection := communication.HttpConnection{}
-	connection.Init("http://127.0.0.1", "5000")
+	connection := communication.TestConnection{}
+	connection.Init(
+		func(request communication.Request) (communication.Response, error) {
+			return communication.Response{ResultCode: 200, Body: map[string]interface{}{}}, nil
+		},
+	)
 	instance := LinearMovement{}
 
 	// THEN
 	err := instance.Init(create_cfg, motors_mapping, steps_per_unit, time_divider, &connection)
 	assert.Equal(t, nil, err)
 
-	err = instance.Move(test_movement, test_feed)
+	err = instance.Move(createChordMovements(10, 50), test_feed)
 	assert.Equal(t, nil, err)
 
 	err = instance.Uninit()
