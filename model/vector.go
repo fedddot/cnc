7089399b7a1781fe29dd279d8e6d22e6561590e0
fedddot <idanrommm@gmail.com>
2024-10-22 09:52:15 +0000

    method added, receiveds updated

--- model/vector.go
+++ model/vector.go
@@ -1,5 +1,7 @@
 package model
 
+import "math"
+
 type Dimension uint
 
 const (
@@ -24,11 +26,11 @@ func (i *Vector[T]) Init(x, y, z T) {
 	}
 }
 
-func (i *Vector[T]) Get(dimension Dimension) T {
+func (i Vector[T]) Get(dimension Dimension) T {
 	return i.coordinates[dimension]
 }
 
-func (i *Vector[T]) Add(other Vector[T]) Vector[T] {
+func (i Vector[T]) Add(other Vector[T]) Vector[T] {
 	result := Vector[T]{}
 	result.Init(
 		i.Get(X)+other.Get(X),
@@ -38,11 +40,19 @@ func (i *Vector[T]) Add(other Vector[T]) Vector[T] {
 	return result
 }
 
-func (i *Vector[T]) Equals(other Vector[T]) bool {
+func (i Vector[T]) Equals(other Vector[T]) bool {
 	for _, dim := range []Dimension{X, Y, Z} {
 		if i.Get(dim) != other.Get(dim) {
 			return false
 		}
 	}
 	return true
+}
+
+func (i Vector[T]) Norm() T {
+	sqr_norm := T(0)
+	for _, dim := range []Dimension{X, Y, Z} {
+		sqr_norm += i.coordinates[dim] * i.coordinates[dim]
+	}
+	return T(math.Sqrt(float64(sqr_norm)))
 }
