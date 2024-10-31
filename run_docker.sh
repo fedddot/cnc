aaeb2f06d4499cb69f95b4cf3ce7c8219ecbadb9
fedddot <idanrommm@gmail.com>
2024-10-31 15:14:51 +0000

    circular movement created

--- run_docker.sh
+++ run_docker.sh
@@ -1,1 +1,1 @@
-docker run -it --user 0:0 --network=host -v ~/dev/cnc:/usr/src/app --name dev_cnc dev_cnc_img /bin/bash
+docker run -it --user 1000:1000 --network=host -d -v ~/dev/cnc:/usr/src/app --name dev_cnc dev_cnc_img /bin/bash
