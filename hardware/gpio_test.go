49d6fa312428956085e2812de47df1b6229af9d6
fedddot <idanrommm@gmail.com>
2024-11-30 07:35:26 +0000

    test updated

--- hardware/gpio_test.go
+++ hardware/gpio_test.go
@@ -2,32 +2,24 @@ package hardware
 
 import (
 	"cnc/client/communication"
-	"encoding/json"
-	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
-func run_test_request(request communication.Request) (communication.Response, error) {
-	body, _ := json.Marshal(request.Body)
-	fmt.Printf("\n\nrunning %s request on route %s with body:\n%s\n", request.Method, request.Route, body)
-	return communication.Response{ResultCode: 200, Body: nil}, nil
-}
-
 func Test_Gpio_Init_Uninit(t *testing.T) {
 	// GIVEN
-	connection := communication.TestConnection{}
+	connection := communication.HttpConnection{}
 	test_create_cfg := GpioCreateConfig{
 		Id: "test_gpio",
 		Config: GpioConfig{
 			GpioId:    17,
-			Direction: GpioDirection(CW),
+			Direction: GpioDirection(IN),
 		},
 	}
 
 	// WHEN
-	connection.Init(run_test_request)
+	connection.Init("http://127.0.0.1", "5000")
 	instance := Gpio{}
 
 	// THEN
