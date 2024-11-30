e5f193c2df616322823042a29bb3d108e68dd007
fedddot <idanrommm@gmail.com>
2024-11-30 09:51:11 +0000

    stepper motor updated

--- hardware/stepper_motor.go
+++ hardware/stepper_motor.go
@@ -2,6 +2,7 @@ package hardware
 
 import (
 	"cnc/client/communication"
+	"encoding/json"
 	"fmt"
 )
 
@@ -58,12 +59,65 @@ func (i *StepperMotor) Init(id StepperMotorId, config StepperMotorConfig, connec
 		return err
 	}
 
-	// TODO: init also the stepper
+	request_body := map[string]interface{}{
+		"id":     i.id,
+		"config": i.generateStepperConfig(),
+	}
+	request := communication.Request{
+		Route:  "steppers",
+		Method: "POST",
+		Body:   request_body,
+	}
+	resp, err := i.connection.RunRequest(request)
+	if err != nil {
+		return err
+	}
+	if resp.ResultCode != 200 {
+		response_body, _ := json.Marshal(resp.Body)
+		return fmt.Errorf("server returned failure code: %d; %s", resp.ResultCode, response_body)
+	}
 	return nil
 }
 
+func (i StepperMotor) generateStepperConfig() interface{} {
+	control_outputs := make(map[ControlOutput]interface{}, 0)
+	for control_tag, _ := range i.config.ControlOutputs {
+		control_outputs[control_tag] = fmt.Sprintf("%s%s%s", i.id, i.delimeter, control_tag)
+	}
+	direction_outputs := make(map[DirectionOutput]interface{}, 0)
+	for direction_tag, _ := range i.config.DirectionOutputs {
+		direction_outputs[direction_tag] = fmt.Sprintf("%s%s%s", i.id, i.delimeter, direction_tag)
+	}
+	states := []map[DirectionOutput]GpioState{
+		{
+			ATop: HIGH,
+			ABtm: LOW,
+			BTop: LOW,
+			BBtm: LOW,
+		},
+	}
+	return map[string]interface{}{
+		"control_outputs":   control_outputs,
+		"direction_outputs": direction_outputs,
+		"states":            states,
+	}
+}
+
 func (i *StepperMotor) Uninit() error {
-	// TODO: init also the stepper
+	request_body := make(map[string]interface{}, 0)
+	request := communication.Request{
+		Route:  fmt.Sprintf("steppers/%s", i.id),
+		Method: "DELETE",
+		Body:   request_body,
+	}
+	resp, err := i.connection.RunRequest(request)
+	if err != nil {
+		return err
+	}
+	if resp.ResultCode != 200 {
+		response_body, _ := json.Marshal(resp.Body)
+		return fmt.Errorf("server returned failure code: %d; %s", resp.ResultCode, response_body)
+	}
 	return i.uninitGpos()
 }
 
